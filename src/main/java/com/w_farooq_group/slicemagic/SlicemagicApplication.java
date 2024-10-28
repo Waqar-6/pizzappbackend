f4c92e24999975b7c458a0697677aafbb6e511d6
Waqar <waqar-1@hotmail.co.uk>
2024-10-28 21:52:33 +0000

    initial commit

---
+++ src/main/java/com/w_farooq_group/slicemagic/SlicemagicApplication.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.slicemagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlicemagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlicemagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w_farooq_group/slicemagic/SlicemagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.slicemagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlicemagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
