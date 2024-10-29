9bb2337f8c86d11c054915b0100feba43d4da243
Waqar <waqar-1@hotmail.co.uk>
2024-10-29 02:25:15 +0000

    constants file

---
+++ src/main/java/com/w_farooq_group/slicemagic/constants/PizzaConstants.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.slicemagic.constants;
+
+public final class PizzaConstants {
+
+    private PizzaConstants () {}
+
+    public static final String PIZZA_MSG_201 = "Pizza created successfully";
+    public static final String PIZZA_MSG_200_UPDATE = "Pizza updated successfully";
+    public static final String PIZZA_MSG_200_DELETE = "Pizza deleted successfully";
+    public static final String PIZZA_MSG_417_UPDATE = "Could not update Pizza due to Expectation fail";
+    public static final String PIZZA_MSG_417_DELETE = "Could not delete Pizza due to Expectation fail";
+
+}
---
+++ src/main/java/com/w_farooq_group/slicemagic/constants/ResponseStatusConstants.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.slicemagic.constants;
+
+public final class ResponseStatusConstants {
+
+
+    private ResponseStatusConstants () {}
+
+    public static final String STATUS_200 = "200";
+    public static final String STATUS_201 = "201";
+    public static final String STATUS_417 = "417";
+
+
+}
